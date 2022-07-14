ad267af945e43d881a387a900340dd8a5ffc7a4a
Aditya Restu Adji <adit.restu77@gmail.com>
2022-07-14 14:03:21 +0700

    new button

--- components/sidemenu.php
+++ components/sidemenu.php
@@ -1,14 +1,27 @@
 <div id="sidemenu" class="bg-white border-end d-flex flex-column" style="width: 15rem; justify-content: space-between;">
   <div class="nav nav-pills flex-column p-3">
     <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+      <a class="nav-link" aria-current="page" href="?p=beranda">Beranda</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Jepang</a>
     </li>
     <li class="nav-item">
+      <a class="nav-link" href="#">Lainnya</a>
+    </li>
+    <hr>
+    <li class="nav-item">
+      <a class="nav-link" href="#">About</a>
+    </li>
+    <li class="nav-item">
       <a class="nav-link" href="#">Request</a>
     </li>
+    <?php if(isset($_SESSION['username'])) : ?>
+      <li class="nav-item">
+        <a class="nav-link" href="?p=stricted">Dashboard</a>
+      </li>
+    <?php endif; ?>
+    <hr>
   </div>
 
   <div class="d-flex p-3 align-items-center justify-content-end">
